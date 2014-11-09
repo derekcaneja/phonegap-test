8b69a50ed1731edfd449777ca8932e9c0ed251e3
trinityXmontoya <trinityXmontoya@gmail.com>
2014-11-08 20:18:35 -0500

    work with ride submit form

---
+++ www/js/googleAutocomplete.js
@@ -0,0 +1,11 @@
+function initialize() {
+  autocomplete0 = new google.maps.places.Autocomplete(
+      (document.getElementsByName('origin')[0]),
+      { types: ['(cities)'] });
+  autocomplete1 = new google.maps.places.Autocomplete(
+      (document.getElementsByName('destination')[0]),
+      { types: ['(cities)'] });
+  autocomplete2 = new google.maps.places.Autocomplete(
+      (document.getElementsByName('pick_up_location')[0]),
+      { types: ['geocode'] });
+}
